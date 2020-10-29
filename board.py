5c9266e72d4584bdfb891aced171d9e4d4791565
jeromestrada <jeromolimac.estrada@sjsu.edu>
2020-10-28 21:11:33 -0700

    Tied the board and driver up, testing simple movement

--- board.py
+++ board.py
@@ -1,21 +1,27 @@
-import sys, pygame
+import pygame
+import sys
+
 pygame.init()
 
-#set game screen size
+# set game screen size
 width = 1280
 height = 720
 size = width, height
 screen = pygame.display.set_mode(size)
 
-#keeps screen printed until game is quit
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-        
-    #make screen background white
+
+
+
+# keeps screen printed until game is quit
+def displayboard():
+    #for event in pygame.event.get():
+       # if event.type == pygame.QUIT: sys.exit()
+
+
+    # make screen background white
     screen.fill(pygame.Color(255, 255, 255, 255))
-    
-    #draw border
+
+    # draw border
     startX = 310
     startY = 30
     endX = 950
@@ -26,27 +32,28 @@ while 1:
     pygame.draw.rect(screen, (0, 0, 0, 0), [startX, startY, thick, length])
     pygame.draw.rect(screen, (0, 0, 0, 0), [startX, endY, length, thick])
     pygame.draw.rect(screen, (0, 0, 0, 0), [endX, startY, thick, length])
-    
-    #draw rectangles
-    hor = 350                           #starting horizontal pos of rectangle
-    ver = 70                            #starting vertical pos of rectangle
-    sq_width = 100                      #rectangle width 
-    sq_height = 100                     #rectangle height
-    square_size = (sq_width, sq_height) #var combining both rectangle width and height
-    
-    for y in range(5):                  #loops through all vertical positions
-        for x in range(5):              #loops through all horizontal positions
-            #resets pos of each rectangle
-            pos = (hor, ver)
-            #draws rectangle
-            pygame.draw.rect(screen, (0, 0, 0, 0), [pos, square_size])
-            #increments hor position
-            hor = hor + 120
-        #resets hor position for each row
-        hor = 350
-        #increments ver position
-        ver = ver + 120
-        
-    #update display    
-    pygame.display.flip()
 
+    # draw rectangles
+    hor = 350  # starting horizontal pos of rectangle
+    ver = 70  # starting vertical pos of rectangle
+    sq_width = 100  # rectangle width
+    sq_height = 100  # rectangle height
+    square_size = (sq_width, sq_height)  # var combining both rectangle width and height
+
+    for y in range(5):  # loops through all vertical positions
+        for x in range(5):  # loops through all horizontal positions
+            # resets pos of each rectangle
+            pos = (hor, ver)
+            # draws rectangle
+            pygame.draw.rect(screen, (0, 0, 0, 0), [pos, square_size])
+            # increments hor position
+            hor = hor + 120
+        # resets hor position for each row
+        hor = 350
+        # increments ver position
+        ver = ver + 120
+
+
+
+    # update display
+    #pygame.display.flip()
--- driver.py
+++ driver.py
@@ -1,19 +1,36 @@
+import time
+
 import pygame
 # from board import Board
 # from snake import Snake
 # from food import Food
 from highscore import HighScore
+import board
+import cube
 
 
 def main():
     pygame.init()
+
+    testimg = pygame.image.load('Perfect25px.png')
+    snekopen = pygame.image.load('OpenMouth25px.png')
+    snekclos = pygame.image.load('ClosedMouth25px.png')
+    snekbadi = pygame.image.load('Body25px.png')
+    snektel = pygame.image.load('Tail25px.png')
+
+    snakex = 350
+    snakey = 100
+
+    dx = 50
+    dy = 10;
+
     gaming = True
 
     # High Score constructor
     score = HighScore()
 
     # Class Board constructor
-    # board = Board() 
+    # board = Board()
 
     # Class Snake constructor
     # snake = Snake()
@@ -21,22 +38,35 @@ def main():
     # Class Food constructor
     # food = Food()
 
+
     while gaming:
-    	# Handle Events
-    	for event in pygame.event.get():
+        board.displayboard()
+        # Handle Events
+        for event in pygame.event.get():
 
-    		# Quit Game
-    		if event.type == pygame.QUIT:
-    			gaming = False
+            # Quit Game
+            if event.type == pygame.QUIT:
+                gaming = False
 
-    		if event.type == pygame.KEYDOWN:
-    			print(event)
-    			# case/switch for up,down,left,right
-    			# add appropriate offset to snake coords (head)
+            if event.type == pygame.KEYDOWN:
+                print(event)
+                if event.key == pygame.K_RIGHT:
+                    snakex += dx
+                # case/switch for up,down,left,right
+                # add appropriate offset to snake coords (head)
 
-    	# Boundary check snake -> Edges and self
-    	# Check if snake ate food -> Increase snake length, move food coord
-    	# Update board
+        # Boundary check snake -> Edges and self
+        # Check if snake ate food -> Increase snake length, move food coord
+        # Update board
+
+
+        cube.put(board.screen, 600, 100, testimg)
+        cube.put(board.screen, snakex, 100, snektel)
+        cube.put(board.screen, snakex + 50, 100, snekbadi)
+        cube.put(board.screen, snakex + 50 + 40, 95, snekopen)
+
+        pygame.display.update()
+
 
     # Game Over
     pygame.quit()
