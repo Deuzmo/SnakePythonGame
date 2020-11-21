ec75a5d90812dc7088b0db6b94c2e1d3caf6ee37
tanishababic <tanisha.babic@sjsu.edu>
2020-11-21 11:58:06 -0800

    Integrated Daniel's score code with the high score code

---
+++ score_curr.py
@@ -0,0 +1,21 @@
+import pygame
+import highscore
+
+pygame.font.init()
+
+
+class Score_Curr:
+	curr = 0
+	txt = None
+
+	def __init__(self):
+		self.txt = self.make_txt(self.curr)
+
+	def make_txt(self, score):
+		txt_str = 'Your Score is: {}'.format(score)
+		txt = pygame.font.SysFont('Comic Sans MS', 25)
+		return txt.render(txt_str, False, (0, 0, 0))
+	
+	def update(self):
+		self.curr += 1
+		self.txt = self.make_txt(self.curr)
